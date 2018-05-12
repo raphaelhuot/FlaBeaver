4ab742bb5da0e647a59b8e45b710f13fbb3025c5
Dimdim2212 <dimitri.arce@gmail.com>
2018-05-12 10:51:03 -0400

    Animation

---
+++ FlaBeaverUnity/Assets/Scripts/Bird.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour {
+    public bool isDead = false;
+
+    private Rigidbody2D rb2d;
+    private Animator anim;
+    // Use this for initialization
+    void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    private void OnCollisionEnter2D()
+    {
+        Debug.Log("Suck it");
+        isDead = true;
+        rb2d.velocity = Vector2.zero;
+        anim.SetTrigger("Die");
+        GameControl.Instance.die();
+    }
+}
--- FlaBeaverUnity/Assets/Scripts/Controller.cs
+++ FlaBeaverUnity/Assets/Scripts/Controller.cs
@@ -7,16 +7,20 @@ public class Controller : MonoBehaviour {
     public MicrophoneScript mic;
     public GameObject beaver;
     private Rigidbody2D beaverRB;
+    public GameObject bird;
+    private Animator anim;
 
     private void Start()
     {
         beaverRB = beaver.GetComponent<Rigidbody2D>();
+        anim = bird.GetComponent<Animator>();
     }
     private void Update()
     {
         if (mic.loudness > 8)
         {
             beaverRB.velocity = new Vector2(beaverRB.velocity.x, 1 + (mic.loudness/4));
+            anim.SetTrigger("Flap");
         }
     }
 }
