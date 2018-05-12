c97e84870249c32ede2225cfeb01b6283451c796
Raphael Huot <r.huot@student.isartdigital.com>
2018-05-11 21:59:49 -0400

    Microphone Working
    C'est fou boi

---
+++ FlaBeaverUnity/Assets/Scripts/Controller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+    public MicrophoneScript mic;
+    public GameObject beaver;
+    private Rigidbody2D beaverRB;
+
+    private void Start()
+    {
+        beaverRB = beaver.GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        if (mic.loudness > 8)
+        {
+            beaverRB.velocity = new Vector2(beaverRB.velocity.x, 4);
+        }
+    }
+}
---
+++ FlaBeaverUnity/Assets/Scripts/MicrophoneScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MicrophoneScript : MonoBehaviour {
+    public float sensitivity = 100;
+    public float loudness = 0;
+    AudioSource myAudio;
+	// Use this for initialization
+	void Start () {
+        myAudio = GetComponent<AudioSource>();
+        myAudio.clip = Microphone.Start(null, true, 10, 44100);
+        myAudio.loop = true;
+        myAudio.mute = true;
+        while (!(Microphone.GetPosition(null) > 0)) { }
+        myAudio.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        loudness = GetAverageVolume() * sensitivity;
+        Debug.Log(loudness);
+	}
+
+    float GetAverageVolume()
+    {
+        float[] data = new float[256];
+        float a = 0;
+        myAudio.GetOutputData(data, 0);
+        foreach(float s in data)
+        {
+            a += Mathf.Abs(s);
+        }
+        return a / 256;
+    }
+}
